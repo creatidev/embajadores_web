d52500b9eb1c4417e2d78a429f04b9950e16b713
Sneyder Sarmiento <redyens@hotmail.com>
2022-03-04 03:08:10 -0500

    Se establece una plantilla temporal para descargar la aplicación.

--- templates/TUsuarios/download.php
+++ templates/TUsuarios/download.php
@@ -1,3 +1,29 @@
-<div>
-    <a>Descargar</a>
+<div class="row">
+    <div class="content center" style="width: 400px">
+        <div class="content center" style="width: 200px">
+            <?php  echo $this->Html->image('logo.png', array('width' => '200px','alt'=>'aswq')); ?>
+        </div>
+        <br>
+        <table>
+
+            <tr>
+                <th>Nombre :</th>
+                <td>Embajadores APP 2.0</td>
+            </tr>
+            <tr>
+                <th>Versión :</th>
+                <td>2.1.0</td>
+            </tr>
+            <tr>
+                <th>Publicado :</th>
+                <td>03/04/2022</td>
+            </tr>
+            <tr>
+                <th>Acción :</th>
+                <td><a href="url">Descargar</a></td>
+            </tr>
+        </table>
+
+    </div>
 </div>
+
